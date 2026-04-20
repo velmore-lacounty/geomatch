d4540a42948254705ab094c669852eddd98c377f
velmore-lacounty <velmore@ph.lacounty.gov>
2026-04-20 13:45:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,3 @@ from server import server
 
 # Create the app using the UI and server components
 app = App(app_ui, server)
-
-# To deploy app to Posit Connect: 
-# rsconnect deploy shiny . --server http://pdlpositcontgis.hosted.lac.com --api-key DQhA57GoaJvSeAGGMPMn9Qy9KoIOKFoc --title "GeoMatch"
